565685f7b2c801bd51bf26a667a8d187e19c1e17
Daniel <daniel.jaramilloe@udea.edu.co>
2018-04-23 22:55:29 -0500

    Cambio del Header2 para modificar el Header

--- include/generic_header2.php
+++ include/generic_header2.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: danie
- * Date: 23/04/2018
- * Time: 22:41
- */
+if (isset($_SESSION['usuario'])) {//Por si ya se hizo sesión anteriormente
+    ?>
+    <div align="right" style="color: green">
+        <img src="images/log.png" width="20px">
+        <?php
+        echo $_SESSION['usuario']['nombre'];
+        ?>
+    </div>
+    <?php
+}else{
+    ?>
+    <div align="right" style="color: red">
+        <img src="images/log.png" width="20px">
+        <?php
+        echo "Inicie sesión";
+        ?>
+    </div>
+    <?php
+}
+?>
+<div align="center">
+    <img src="images/favicon2.png" alt="AntioquiaMide"/>
+</div>
